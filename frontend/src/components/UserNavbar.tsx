25687ea4add24b199bf4a64b2bd888413dd2cc31
chandrikajalluri-wow <chandrikajalluri@wowlabz.com>
2026-01-13 11:41:18 +0530

    fix: fixed the build error.

--- frontend/src/components/UserNavbar.tsx
+++ frontend/src/components/UserNavbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate, NavLink, useLocation } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { getProfile } from '../services/userService';
 import ConfirmationModal from './ConfirmationModal';
 import '../styles/UserNavbar.css';
